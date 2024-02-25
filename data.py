02330b5dd139301e5719e45e37ee46b7b5d30826
Carissa Carriveau <65925888+cmcarriveau@users.noreply.github.com>
2024-02-24 21:33:53 -0700

    View and Add Ingredients
    Added functionality to view the list of ingredients, and added a button that allows the user to add to the database of ingredients.

--- data.py
+++ data.py
@@ -1,10 +1,88 @@
 from pymongo import MongoClient
 
-cluster = "mongodb+srv://carissa:Sj67IbVeK2byWQ9G@clusters.ngx304e.mongodb.net/?retryWrites=true&w=majority&appName=Clusters"
-client = MongoClient(cluster)
+#begins fetching the topping object
+def fetchToppings():
+    data = getToppingData()
+    list = createToppingsList(data)
+    return list
 
-print(client.list_database_names())
+#retrieves data from MongoDB database
+def getToppingData():
+    try:
+        #connect to database
+        cluster = "mongodb+srv://carissa:Sj67IbVeK2byWQ9G@clusters.ngx304e.mongodb.net/?retryWrites=true&w=majority&appName=Clusters"
+        client = MongoClient(cluster)
 
-db = client.test 
+        #access database and collection 
+        db = client["PizzaData"]
+        collection = db["Toppings"]
 
-print(db.list_collection_names())
+        #retrieve data
+        data = collection.find_one()
+
+        #close connection
+        client.close()
+
+        return data
+    
+    #catch any errors
+    except Exception as e:
+        print("Error: ", e)
+
+#convert json file into object
+def createToppingsList(data):
+
+    toppingList = Toppings(
+        id = data['id'],
+        type=data['type'],
+        sauces=data['sauces'],
+        cheeses=data['cheeses'],
+        meats=data['meats'],
+        veggies=data['veggies']
+    )
+
+    return toppingList
+
+def addData(type, name):
+    try:
+        #connect to database
+        cluster = "mongodb+srv://carissa:Sj67IbVeK2byWQ9G@clusters.ngx304e.mongodb.net/?retryWrites=true&w=majority&appName=Clusters"
+        client = MongoClient(cluster)
+
+        #access database and collection 
+        db = client["PizzaData"]
+        collection = db["Toppings"]
+
+        document = collection.find_one({"id": "1"})
+
+        if document:
+            newTopping = {"name": name}
+            document[type].append(newTopping)
+
+            collection.update_one({"_id": document["_id"]}, {"$set": document})
+            print(name, "was added to", type)
+        else:
+            print("Not found")
+
+
+        #close connection
+        client.close()
+    
+    #catch any errors
+    except Exception as e:
+        print("Error: ", e)
+
+class Topping:
+    def __init__(self, name):
+        self.name = name
+
+#Toppings class where the data will be stored
+class Toppings:
+    #initializr class
+    def __init__(self, id, type, sauces, cheeses, meats, veggies):
+        self.id = id
+        self.type = type
+        self.sauces = [Topping(sauce['name']) for sauce in sauces]
+        self.cheeses = [Topping(cheese['name']) for cheese in cheeses]
+        self.meats = [Topping(meat['name']) for meat in meats]
+        self.veggies = [Topping(veggie['name']) for veggie in veggies]
--- pizzaGUI.py
+++ pizzaGUI.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import *
+from data import fetchToppings, addData
 
 #class for displaying selected page
 class tkinterManager(tk.Tk):
@@ -31,6 +33,7 @@ class tkinterManager(tk.Tk):
 
         self.display(HomePage)
 
+
 #home page layout
 class HomePage(tk.Frame):
     #initalize home page class
@@ -55,6 +58,53 @@ class HomePage(tk.Frame):
         chefBtn = Button(self, text="Pizza Chef", fg="red", command=lambda: controller.display(ChefPage))
         chefBtn.grid(row=5, column=0, pady=10, padx=10)
 
+
+#function called to load and reload toppings list
+def loadToppings():
+    toppings = fetchToppings()
+    return toppings
+
+def addToppingWindow():
+    def addTopping():
+        name = newTopping.get()
+        type = selected.get()
+        addData(type, name)
+        add_topping_window.destroy()
+
+    #pop open new window
+    add_topping_window = tk.Toplevel(root)
+    add_topping_window.title("Add")
+    add_topping_window.transient(root)
+    add_topping_window.grab_set()
+    add_topping_window.geometry("400x300")
+
+    #title label
+    addTitle = Label(add_topping_window, text="Add Topping")
+    addTitle.config(font=('Helvetica bold', 18))
+    addTitle.grid(row=0, column=0, pady=10, padx=10)
+
+    #select type of topping
+    typeLabel = Label(add_topping_window, text="Select Topping Type:")
+    typeLabel.grid(row=1, column=0, pady=10, padx=10)
+
+    #combo box for selecting topping type
+    selected = StringVar()
+    toppingCombo = ttk.Combobox(add_topping_window, textvariable=selected, state='readonly')
+    toppingCombo['values'] = ('sauces', 'cheeses', 'meats', 'veggies')
+    toppingCombo.grid(row=1, column=1, pady=10, padx=10)
+
+    #label for new topping
+    entryLabel = Label(add_topping_window, text="Topping Name:")
+    entryLabel.grid(row=2, column=0, pady=10, padx=10)
+
+    #text field for new topping
+    newTopping = Entry(add_topping_window)
+    newTopping.grid(row=2, column=1, pady=10, padx=10)
+
+    #add button
+    addTBtn = Button(add_topping_window, text="Add", command=addTopping)
+    addTBtn.grid(row=3, column=1, pady=10, padx=10)
+
 #owner page layout
 class OwnerPage(tk.Frame):
     #initalize owner page class
@@ -70,6 +120,57 @@ class OwnerPage(tk.Frame):
         title = Label(self, text="Topping Manager")
         title.config(font=('Helvetica bold', 18))
         title.grid(row=1, column=1, padx=10)
+
+        #add button
+        addBtn = Button(self, text="Add", command=addToppingWindow)
+        addBtn.grid(row=2, column=1, pady=10, padx=10)
+        #edit button 
+
+        #delete button
+
+        #load list
+        toppings = loadToppings()
+
+        #print list
+        #sauce label
+        saucesLabel = Label(self, text="Sauces:")
+        saucesLabel.grid(row=3, column=0, pady=10, padx=10)
+        #sauce listbox
+        sauceNum = len(toppings.sauces)
+        saucesListbox = Listbox(self, height=sauceNum)
+        for sauce in toppings.sauces:
+            saucesListbox.insert(END, sauce.name)
+        saucesListbox.grid(row=3, column=1, pady=10, padx=10)
+
+        #cheese label
+        cheesesLabel = Label(self, text="Cheeses:")
+        cheesesLabel.grid(row=4, column=0, pady=10, padx=10)
+        #cheese listbox
+        cheeseNum = len(toppings.cheeses)
+        cheesesListbox = Listbox(self, height=cheeseNum)
+        for cheese in toppings.cheeses:
+            cheesesListbox.insert(END, cheese.name)
+        cheesesListbox.grid(row=4, column=1, pady=10, padx=10)
+
+        #meat label
+        meatsLabel = Label(self, text="Meats:")
+        meatsLabel.grid(row=5, column=0, pady=10, padx=10)
+        #meat listbox
+        meatsNum = len(toppings.meats)
+        meatsListbox = Listbox(self, height=meatsNum)
+        for meat in toppings.meats:
+            meatsListbox.insert(END, meat.name)
+        meatsListbox.grid(row=5, column=1, pady=10, padx=10)
+
+        #veggie label
+        veggiesLabel = Label(self, text="Veggies:")
+        veggiesLabel.grid(row=6, column=0, pady=10, padx=10)
+        #veggie listbox
+        veggieNum = len(toppings.veggies)
+        veggiesListbox = Listbox(self, height=veggieNum)
+        for veggie in toppings.veggies:
+            veggiesListbox.insert(END, veggie.name)
+        veggiesListbox.grid(row=6, column=1, pady=10, padx=10)
 
 
 #chef page layout
@@ -91,4 +192,5 @@ class ChefPage(tk.Frame):
         #function buttons 
 
 root = tkinterManager()
+root.minsize(width=350, height=500)
 root.mainloop()
